9ecbd6c133358706a97df769487419bd527f2a4e
Raaccoon <mandrelierv@yahoo.com>
2020-07-06 15:33:47 +0200

    Création de la structure des controller

---
+++ Back-end/TicketController.php
@@ -0,0 +1,31 @@
+<?php
+namespace Controller;
+use Helper;
+
+class TicketController{
+
+    // Création d'un nouveau ticket
+    static function Create(array $param){  
+              
+    }
+
+    // Récupération de tous les ticket
+    static function GetAllTickets(){
+
+    }
+
+    //Récupération d'un ticket précis
+    static function GetTicket($id){
+
+    }
+
+    //Mise à jour du ticket
+    static function Update($id, array $param){      
+
+    }
+
+    //Suppression du ticket
+    static function Delete($id){
+
+    }
+}
