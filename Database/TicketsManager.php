01c7d314408574d00f4271f001f41e18b4605b7f
Raaccoon <mandrelierv@yahoo.com>
2020-07-07 10:35:56 +0200

    mdofi maj

--- Database/TicketsManager.php
+++ Database/TicketsManager.php
@@ -5,13 +5,13 @@ class TicketsManager extends Model{
 
   //récupère tous les tickets
   public function getAllTickets(){
-    return $this->getAll('ticket', 'Ticket');
+    return $this->getAll('TICKET', 'Ticket');
   }
 
   //récupère un ticket par rapport à son id
   public function getTicket($id)
   {
-    $req = $this->getBdd()->prepare('SELECT * FROM ticket WHERE T_ID='.$id);
+    $req = $this->getBdd()->prepare('SELECT * FROM TICKET WHERE T_ID='.$id);
     $req->execute();
     $var = $req->fetch(PDO::FETCH_ASSOC);
     return $var;
@@ -19,14 +19,14 @@ class TicketsManager extends Model{
 
   //modifie le ticket avec l'id correspondant + retourne le ticket modifié
   public function updateTicket($id, array $param){
-    $req = $this->getBdd()->prepare("UPDATE ticket SET T_NUMERO = '".$param[0]."', T_DATE_SAISIE = '".$param[1]."', T_DESCRIPTION = '".$param[2]."', MATERIEL_M_ID = '".$param[3]."', ETAT_E_CODE = '".$param[4]."' WHERE T_ID = ".$id);
+    $req = $this->getBdd()->prepare("UPDATE TICKET SET T_NUMERO = '".$param[0]."', T_DATE_SAISIE = '".$param[1]."', T_DESCRIPTION = '".$param[2]."', MATERIEL_M_ID = '".$param[3]."', ETAT_E_CODE = '".$param[4]."' WHERE T_ID = ".$id);
     $req->execute();
     return getTicket($id);
   }
 
   //supprime le ticket avec l'id correspondant + retourne un true si supression effectué et false sinon
   public function deleteTicket($id){
-    $req = $this->getBdd()->prepare("DELETE FROM ticket WHERE T_ID = ".$id);
+    $req = $this->getBdd()->prepare("DELETE FROM TICKET WHERE T_ID = ".$id);
     $count = $req->execute();
     if($count != 0){
       return true;
