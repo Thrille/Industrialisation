4a46e5f10bdc0da0ae5af15cacad94fb0c3fd877
Lug Eslan <32539149+Kymareas@users.noreply.github.com>
2020-07-07 17:06:57 +0200

    Image + début config Babel

---
+++ src/script/APIAdapter.js
@@ -0,0 +1,47 @@
+class APIAdapter {
+
+    #apiURL;
+    #authTocken;
+
+    constructor() {
+        this.apiURL = window.location.href + '/api/';
+    }
+
+    async Auth({login, password}) {
+        
+    }
+
+    async ReadTicketList() {
+        return fetch(this.apiURL + 'tickets.php', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + this.authTocken
+            }
+        });
+    }
+
+    async ReadTicketById(id) {
+        return fetch(this.apiURL + 'ticket.php?id=' + id, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + this.authTocken
+            }
+        });
+    }
+
+    async CreateNewTicket({number, description, stateCode, deviceCode}) {
+        
+    }
+
+    async UpdateTicketById({id, number, description, stateCode, deviceCode}) {
+        
+    }
+
+    async DeleteTicketById(id) {
+        
+    }
+};
+
+export default APIAdapter;
