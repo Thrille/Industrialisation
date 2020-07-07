c388a782d6d5786d756e4a80778ecd9e91a2e8fc
Lug Eslan <32539149+Kymareas@users.noreply.github.com>
2020-07-07 09:44:44 +0200

    correction des commentaires

--- Database/.env.example.php
+++ Database/.env.example.php
@@ -1,6 +1,6 @@
 <?php
     // définition des variables d'environnement du projet
-    // ce fichier est un fichier d'exemple, copiez le sous le nom env.php puis remplacez les valeurs
+    // ce fichier est un fichier d'exemple, copiez le sous le nom .env.php puis remplacez les valeurs
 
     // variables pour la connection base de donnée
     define('DB_HOST', 'localhost');
