b1ec3bdb38a9ac1e4c538069ca16f71e143844da
Thrille <mandrelierv@yahoo.com>
2020-07-08 13:13:57 +0200

    Create .env.php

---
+++ Database/.env.php
@@ -0,0 +1,11 @@
+<?php
+    // définition des variables d'environnement du projet
+    // ce fichier est un fichier d'exemple, copiez le sous le nom .env.php puis remplacez les valeurs
+
+    // variables pour la connection base de donnée
+    define('DB_HOST', 'mysql-thrille.alwaysdata.net');
+    define('DB_NAME', 'thrille_ticketing');
+    define('DB_CHARSET', 'UTF8');
+    define('DB_USER', 'thrille');
+    define('DB_PASSWORD', 'Deraper18');
+    define('DB_PORT', '3306');
